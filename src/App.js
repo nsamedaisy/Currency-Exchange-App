5577c49e038048daf98676d45aeac380846d675d
nsamedaisy <nsamedaisy@gmail.com>
2023-07-01 07:56:02 +0100

    my currency exchange commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Wallet } from "./pages/Wallet";
+import { Route, Routes } from "react-router-dom";
+import { CurrencyContext } from "./utils/context";
+
+const baseUrl =
+"https://api.currencyapi.com/v3/latest?apikey=1CsPRodQTXYJSfhYvluRYzFUIp532gq9lQ3A9D5X";
 
 function App() {
+  const [currencies, setCurrencies] = useState(null);
+  const [baseCurrency, setBaseCurrency] = useState([]);
+
+  const updateBaseCurrency = (data) => {
+    ["USD", "EUR", "XAF"].forEach((cur) => {
+      setBaseCurrency((prev) => [...prev, data[`${cur}`]]);
+    });
+  };
+
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => {
+      setCurrencies(response.data);
+      updateBaseCurrency(response.data.data);
+    });
+  }, []);
+
+
   return (
-    <div className="App">app
-    </div>
+    <CurrencyContext.Provider value={{ baseCurrency }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="wallet" element={<Wallet />} />
+      </Routes>
+    </CurrencyContext.Provider>
   );
 }
 
