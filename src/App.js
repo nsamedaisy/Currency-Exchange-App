806d0a6dd5528615b2de6da421dca802903aa466
nsamedaisy <nsamedaisy@gmail.com>
2023-07-02 22:53:10 +0100

    currency exchange commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,105 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import Axios from "axios";
+import Dropdown from "react-dropdown";
+import { HiSwitchHorizontal } from "react-icons/hi";
+import "react-dropdown/style.css";
 import "./App.css";
-import { Home } from "./pages/Home";
-import { Wallet } from "./pages/Wallet";
-import { Route, Routes } from "react-router-dom";
-import { CurrencyContext } from "./context/Context";
-
-const baseUrl =
-"https://api.currencyapi.com/v3/latest?apikey=1CsPRodQTXYJSfhYvluRYzFUIp532gq9lQ3A9D5X";
 
 function App() {
-  const [currencies, setCurrencies] = useState(null);
-  const [baseCurrency, setBaseCurrency] = useState([]);
+  // Initializing all the state variables
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
 
-  const updateBaseCurrency = (data) => {
-    ["USD", "EUR", "XAF"].forEach((cur) => {
-      setBaseCurrency((prev) => [...prev, data[`${cur}`]]);
-    });
-  };
-
+  // Calling the api whenever the dependency changes
   useEffect(() => {
-    axios.get(baseUrl).then((response) => {
-      setCurrencies(response.data);
-      updateBaseCurrency(response.data.data);
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
+    ).then((res) => {
+      setInfo(res.data[from]);
     });
-  }, []);
+  }, [from]);
 
+  // Calling the convert function whenever
+  // a user switches the currency
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    convert();
+  }, [info]);
+
+  // Function to convert the currency
+  function convert() {
+    var rate = info[to];
+    setOutput(input * rate);
+  }
+
+  // Function to switch between two currency
+  function flip() {
+    var temp = from;
+    setFrom(to);
+    setTo(temp);
+  }
 
   return (
-    <CurrencyContext.Provider value={{ baseCurrency }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="wallet" element={<Wallet />} />
-      </Routes>
-    </CurrencyContext.Provider>
+    <div className="App">
+      <div className="heading">
+        <h1>Currency Exchange</h1>
+      </div>
+      <div className="container">
+        <div className="left">
+          <h3>Amount</h3>
+          <input
+            type="text"
+            placeholder="Enter the amount"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <div className="middle">
+          <h3>From</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => {
+              setFrom(e.value);
+            }}
+            value={from}
+            placeholder="From"
+          />
+        </div>
+        <div className="switch">
+          <HiSwitchHorizontal
+            size="30px"
+            onClick={() => {
+              flip();
+            }}
+          />
+        </div>
+        <div className="right">
+          <h3>To</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => {
+              setTo(e.value);
+            }}
+            value={to}
+            placeholder="To"
+          />
+        </div>
+      </div>
+      <div className="result">
+        <button
+          onClick={() => {
+            convert();
+          }}
+        >
+          Convert
+        </button>
+        <h2>Converted Amount:</h2>
+        <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+      </div>
+    </div>
   );
 }
 
